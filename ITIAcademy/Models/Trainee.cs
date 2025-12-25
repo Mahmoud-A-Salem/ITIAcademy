b153a038b27546997e40dfb8b19d095608ae5db0
‪Mahmoud Ali Salem‬‏ <94777124+Mahmoud-A-Salem@users.noreply.github.com>
2025-12-25 14:55:35 +0200

    Add validation attributes to Trainee model
    Introduced data annotations for the Trainee model properties to enforce required fields, string length constraints, and image URL format validation. This enhances model validation and improves data integrity.

--- ITIAcademy/Models/Trainee.cs
+++ ITIAcademy/Models/Trainee.cs
@@ -1,10 +1,18 @@
-﻿namespace ITIAcademy.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITIAcademy.Models
 {
     public class Trainee
     {
+        [Required]
         public int Id { get; set; }
+        [Required(ErrorMessage = "The name of the Trainee is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [RegularExpression(@".\.(jpg|png)")]
         public string? ImageURL { get; set; }
+        [Required(ErrorMessage = "The address is required")]
         public string Address { get; set; }
         public string Grade { get; set; }
 
